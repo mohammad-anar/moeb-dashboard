315a814e2dcbc581afa222087f9130158d6b2bbe
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-02-02 02:23:41 +0600

    app-sidebar added

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,145 +1,29 @@
 "use client";
 
-import * as React from "react";
 import {
   IconBrandProducthunt,
-  IconCamera,
   IconCategory,
-  IconChartBar,
   IconDashboard,
-  IconDashboardFilled,
-  IconDatabase,
-  IconFileAi,
-  IconFileDescription,
-  IconFileWord,
-  IconFolder,
-  IconHelp,
-  IconInnerShadowTop,
   IconList,
-  IconListDetails,
   IconMenuOrder,
-  IconReport,
-  IconSearch,
   IconSettings,
-  IconUsers,
   IconUsersGroup,
 } from "@tabler/icons-react";
+import * as React from "react";
 
+import logo from "@/assets/logo.png";
 import { NavDocuments } from "@/components/nav-documents";
-import { NavMain } from "@/components/nav-main";
-import { NavSecondary } from "@/components/nav-secondary";
-import { NavUser } from "@/components/nav-user";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarHeader,
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import Link from "next/link";
 import Image from "next/image";
-import logo from "@/assets/logo.png";
+import Link from "next/link";
 
-export const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Dashboard",
-      url: "#",
-      icon: IconDashboard,
-    },
-    {
-      title: "Lifecycle",
-      url: "#",
-      icon: IconListDetails,
-    },
-    {
-      title: "Analytics",
-      url: "#",
-      icon: IconChartBar,
-    },
-    {
-      title: "Projects",
-      url: "#",
-      icon: IconFolder,
-    },
-    {
-      title: "Team",
-      url: "#",
-      icon: IconUsers,
-    },
-  ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: IconCamera,
-      isActive: true,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: IconFileDescription,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: IconFileAi,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "#",
-      icon: IconSettings,
-    },
-    {
-      title: "Get Help",
-      url: "#",
-      icon: IconHelp,
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: IconSearch,
-    },
-  ],
+const data = {
   documents: [
     {
       name: "Dashboard",
@@ -186,11 +70,16 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             {/* <SidebarMenuButton asChild className=" flex items-center border"> */}
-            <div className="flex items-center px-8 pb-2">
-              <Link href="/" className="block w-20 h-20">
+            <div className="flex items-center justify-center px-8 pb-2">
+              <Link
+                href="/"
+                className="block w-28 duration-300 rounded-full overflow-hidden"
+              >
                 <Image
                   src={logo}
                   className="w-full h-full"
+                  width={300}
+                  height={300}
                   alt="Marbapp logo"
                 />
               </Link>

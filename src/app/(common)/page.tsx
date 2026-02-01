acc0f07375509f230efd6df007a0a5acd83acfb8
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-02-02 02:38:17 +0600

    not found page added

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,8 +1,4 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
-import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
-
-import data from "./data.json";
 
 const page = () => {
   return (
@@ -11,10 +7,6 @@ const page = () => {
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <SectionCards />
-            <div className="px-4 lg:px-6">
-              <ChartAreaInteractive />
-            </div>
-            <DataTable data={data} />
           </div>
         </div>
       </div>
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,5 +1,14 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { NavUser } from "./nav-user";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { IconBell } from "@tabler/icons-react";
+import { User } from "lucide-react";
+import Link from "next/link";
 
 export function SiteHeader() {
   const data = {
@@ -20,6 +29,77 @@ export function SiteHeader() {
         /> */}
         <h1 className="text-base font-medium">Documents</h1>
         <div className="ml-auto flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <div
+                className={
+                  " hover:bg-gray-100 duration-300 cursor-pointer rounded-md p-3"
+                }
+              >
+                <div className="relative ">
+                  <div className="w-5 h-5 rounded-full bg-red-500 absolute -right-2 -top-1 flex items-center justify-center">
+                    <span className="text-[10px] text-white">5</span>
+                  </div>
+                  <IconBell size={28} className="h-full" />
+                </div>
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[300px]">
+              <div className="px-2">
+                <h4 className="font-bold text-lg">Notification</h4>
+              </div>
+              <DropdownMenuSeparator />
+              <div className="px-2 max-h-[200px] overflow-y-auto">
+                <div className="flex items-center  gap-3 bg-my-primary/10 hover:bg-gray-100 duration-300 rounded-md px-3 p-2 cursor-pointer">
+                  <User />
+                  <div>
+                    <h4 className="text-sm  font-bold">New order</h4>
+                    <p className="text-sm">2 min ago</p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex items-center  gap-3 hover:bg-gray-100 duration-300 rounded-md px-3 p-2 cursor-pointer">
+                  <User />
+                  <div>
+                    <h4 className="text-sm  font-bold">New order</h4>
+                    <p className="text-sm">2 min ago</p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex items-center  gap-3 hover:bg-gray-100 duration-300 rounded-md px-3 p-2 cursor-pointer">
+                  <User />
+                  <div>
+                    <h4 className="text-sm  font-bold">New order</h4>
+                    <p className="text-sm">2 min ago</p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex items-center  gap-3 hover:bg-gray-100 duration-300 rounded-md px-3 p-2 cursor-pointer">
+                  <User />
+                  <div>
+                    <h4 className="text-sm  font-bold">New order</h4>
+                    <p className="text-sm">2 min ago</p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex items-center  gap-3 hover:bg-gray-100 duration-300 rounded-md px-3 p-2 cursor-pointer">
+                  <User />
+                  <div>
+                    <h4 className="text-sm  font-bold">New order</h4>
+                    <p className="text-sm">2 min ago</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center justify-end p-2 pr-5">
+                <Link
+                  href={"/notifications"}
+                  className="text-sm underline text-blue-500 hover:text-primary"
+                >
+                  See All
+                </Link>
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <NavUser user={data?.user} />
         </div>
       </div>
